c8b2bf172ae4974061b496c85f8ad8db356f3f99
giudsv <giuseppedsv2003@gmail.com>
2025-01-31 15:18:00 +0100

    Fixing Directory

--- DataPreprocessing/dataCleaning/Cleaning.py
+++ DataPreprocessing/dataCleaning/Cleaning.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Caricare il dataset
-df = pd.read_csv("../dataset/ufc-master.csv")
+df = pd.read_csv("../../dataset/ufc-master.csv")
 
 # Lista delle colonne da rimuovere
 cols_to_remove = {
@@ -13,7 +13,7 @@ cols_to_remove = {
 df = df.drop(columns=cols_to_remove, errors="ignore")
 
 
-output_path = "../dataset/finalDataset.csv"
+output_path = "../../dataset/finalDataset.csv"
 
 # Salvare il dataset in un nuovo file CSV
 df.to_csv(output_path, index=False)
